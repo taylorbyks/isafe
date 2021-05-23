7500170625d94e50ab2a778fb68f8c50efe77f68
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-05-23 12:14:13 -0300

    Change next screen

--- src/screens/Welcome/index.js
+++ src/screens/Welcome/index.js
@@ -1,32 +1,33 @@
-import React from "react";
+import React from 'react'
 
-import virus from "../../assets/virus.png";
+import virus from '../../assets/virus.png'
 
-import { Title, Description, IconButton } from "../../components";
-import { WelcomeContainer, MainImage } from "./styles";
+import { Title, Description, IconButton } from '../../components'
+import { WelcomeContainer, MainImage } from './styles'
 
-import { useNavigation } from "@react-navigation/core";
+import { useNavigation } from '@react-navigation/core'
 
 export const Welcome = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation()
 
   function handleNext() {
-    navigation.navigate("Home");
+    navigation.navigate('Logon')
   }
 
   return (
     <WelcomeContainer>
       <Title>
-      Se proteja do {"\n"}
-      Covid-19
+        Se proteja do {'\n'}
+        Covid-19
       </Title>
 
       <MainImage source={virus} />
 
-      <Description>O !Safe vai te mostrar lugares que nao sao seguros, onde a contaminaçao esta alta para você evitar
+      <Description>
+        O !Safe vai te mostrar lugares que nao sao seguros, onde a contaminaçao esta alta para você evitar
       </Description>
 
       <IconButton iconName="chevron-forward-outline" onPress={handleNext} />
     </WelcomeContainer>
-  );
-};
+  )
+}
--- src/screens/Welcome/styles.js
+++ src/screens/Welcome/styles.js
@@ -1,18 +1,17 @@
-import styled, { css } from "styled-components/native";
+import styled, { css } from 'styled-components/native'
 
 export const WelcomeContainer = styled.View`
   ${({ theme }) => css`
     flex: 1;
     align-items: center;
     justify-content: space-between;
-    padding: ${theme.metrics.px(68)}px ${theme.metrics.px(15)}px
-      ${theme.metrics.px(58)}px;
+    padding: ${theme.metrics.px(68)}px ${theme.metrics.px(15)}px ${theme.metrics.px(58)}px;
   `}
-`;
+`
 
 export const MainImage = styled.Image`
   ${({ theme }) => css`
     width: ${theme.metrics.px(350)}px;
     height: ${theme.metrics.px(284)}px;
   `}
-`;
+`
