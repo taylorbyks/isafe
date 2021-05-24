7d922eda69cba283498024717a825b750d945f6b
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-05-24 18:25:45 -0300

    Save city

--- src/screens/Logon/index.js
+++ src/screens/Logon/index.js
@@ -1,28 +1,64 @@
-import React from 'react'
-
-import virus from '../../assets/virus.png'
-
-import { Title, Description, Button } from '../../components'
+import React, { useState }  from 'react'
+import { saveCity } from '../../services/storage'
+import { Title, Description, Button, Input } from '../../components'
 import { LogonContainer } from './styles'
 
 import { useNavigation } from '@react-navigation/core'
+import { Alert } from 'react-native'
+
 
 export const Logon = () => {
   const navigation = useNavigation()
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+  const [city, setCity] = useState()
 
-  function handleNext() {
-    navigation.navigate('Home')
+  function handleInputBlur() {
+    setIsFocused(false)
+    setIsFilled(!!city)
+  }
+
+  function handleInputFocus() {
+    setIsFocused(true)
+  }
+
+  function handleInputChange(value) {
+    setIsFilled(!!value)
+    setCity(value)
+  }
+
+  async function handleSubmit() {
+    if (!city) {
+      return Alert.alert('Me informe sua cidade 😥')
+    }
+
+    try {
+      console.log(city)
+      await saveCity(city)
+
+      navigation.navigate('Home')
+    } catch {
+      return Alert.alert('Não foi possível salvar sua cidade! 😥')
+    }
   }
 
   return (
     <LogonContainer>
       <Title>Qual a sua cidade?</Title>
 
-      <Description>
-        O !Safe vai te mostrar lugares que nao sao seguros, onde a contaminaçao esta alta para você evitar
-      </Description>
+      <Input 
+        placeholder="Digite sua Cidade"
+        selected={isFocused || isFilled}
+        value={setCity}
+        onBlur={handleInputBlur}
+        onFocus={handleInputFocus}
+        onChangeText={handleInputChange}
+      />
+      {/* <Description>
+        Insira para que possamos te mostrar o seu arredor
+      </Description> */}
 
-      <Button text="Confirmar" onPress={handleNext} />
+      <Button text="Confirmar" onPress={handleSubmit} />
     </LogonContainer>
   )
 }
