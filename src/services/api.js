56e8ef1a52d284c39fdda1d0fcad680c9247682a
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-05-24 18:25:53 -0300

    Add static data

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,25 @@
-const ACCESS_TOKEN_MAP_BOX =
-  'pk.eyJ1IjoidGF5bG9yYnlrcyIsImEiOiJja2c3N2t1MTEwMG9tMnhtaHRya2FnOG8xIn0.MbZsKNHOGYQqZU4KJaT3lA'
+import React from 'react'
 
 export const fetchLocalMapBox = (local) =>
   fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?${ACCESS_TOKEN_MAP_BOX}`)
     .then((response) => response.json())
     .then((data) => data)
+
+export const data = [
+  {
+    latitude: -24.95,
+    longitude: -53.45
+  },
+  {
+    latitude: -24.96,
+    longitude: -53.46
+  },
+  {
+    latitude: -24.97,
+    longitude: -53.44
+  },
+  {
+    latitude: -24.975,
+    longitude: -53.47
+  }
+]
