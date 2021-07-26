57ae3e69052f15579a0df22d632d324666ad0878
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-07-26 18:12:42 -0300

    Call api

--- mobile/src/screens/Home/index.js
+++ mobile/src/screens/Home/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 import { HomeContainer, LogoImage } from './styles'
-import { Title, LoadAnimation } from '../../components'
+import { LoadAnimation } from '../../components'
 
 import { Dimensions, StyleSheet } from 'react-native'
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
@@ -9,17 +9,21 @@ import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import covidMarker from '../../assets/covidMarker.png'
 import Logo from '../../assets/logo.png'
 import mapStyle from '../../utils/mapStyle.json'
-import api, { data } from '../../services/api'
+import { getPoints } from '../../services/calls'
 
 export const Home = () => {
   const [loading, setLoading] = useState(true)
-  const [covid, setCovid] = useState(data)
+  const [covid, setCovid] = useState()
+
+  async function getPointsFromApi() {
+    const response = await getPoints()
+    setCovid(response)
+    setLoading(false)
+  }
 
   useEffect(() => {
-    setTimeout(()=>{
-      setLoading(false)
-     }, 600)
-  })
+    getPointsFromApi()
+  }, [])
 
   if (loading) {
     return <LoadAnimation />
@@ -38,18 +42,19 @@ export const Home = () => {
           longitudeDelta: 0.07,
         }}
       >
-        {covid.map(virus => {
+        {covid.map((virus) => {
           return (
             <Marker
-            icon={covidMarker}
-            calloutAnchor={{
-              x: 2.5,
-              y: 0.9,
-            }}
-            coordinate={{
-              latitude: virus.latitude, 
-              longitude: virus.longitude,
-            }}
+              key={virus.uuid}
+              icon={covidMarker}
+              calloutAnchor={{
+                x: 2.5,
+                y: 0.9,
+              }}
+              coordinate={{
+                latitude: virus.latitude,
+                longitude: virus.longitude,
+              }}
             />
           )
         })}
