d68e24860890bf602dde64e4486f28c0621dc74d
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-07-26 18:12:16 -0300

    Add api and calls

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,25 +1,7 @@
-import React from 'react'
+import axios from 'axios'
 
-export const fetchLocalMapBox = (local) =>
-  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?${ACCESS_TOKEN_MAP_BOX}`)
-    .then((response) => response.json())
-    .then((data) => data)
+const api = axios.create({
+  baseURL: 'http://localhost:3000',
+})
 
-export const data = [
-  {
-    latitude: -24.95,
-    longitude: -53.45
-  },
-  {
-    latitude: -24.96,
-    longitude: -53.46
-  },
-  {
-    latitude: -24.97,
-    longitude: -53.44
-  },
-  {
-    latitude: -24.975,
-    longitude: -53.47
-  }
-]
+export default api
---
+++ mobile/src/services/calls.js
@@ -0,0 +1,11 @@
+import api from './api'
+
+export async function getPoints() {
+  try {
+    const result = await api.get('/points')
+
+    return result.data
+  } catch (error) {
+    return error
+  }
+}
