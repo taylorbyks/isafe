5efda974793cf2f6b359302f1a4148c29e4008b0
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-07-26 22:26:25 -0300

    Add new actions

--- mobile/src/screens/Home/index.js
+++ mobile/src/screens/Home/index.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
-import { HomeContainer, LogoImage } from './styles'
-import { LoadAnimation } from '../../components'
+import { HomeContainer, LogoImage, FooterContainer } from './styles'
+import { LoadAnimation, IconButton } from '../../components'
 
 import { Dimensions, StyleSheet } from 'react-native'
+import { useNavigation, useIsFocused } from '@react-navigation/core'
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 
 import covidMarker from '../../assets/covidMarker.png'
@@ -12,8 +13,10 @@ import mapStyle from '../../utils/mapStyle.json'
 import { getPoints } from '../../services/calls'
 
 export const Home = () => {
+  const navigation = useNavigation()
   const [loading, setLoading] = useState(true)
   const [covid, setCovid] = useState()
+  const isFocused = useIsFocused()
 
   async function getPointsFromApi() {
     const response = await getPoints()
@@ -21,9 +24,13 @@ export const Home = () => {
     setLoading(false)
   }
 
+  async function handleAdd() {
+    navigation.navigate('Create')
+  }
+
   useEffect(() => {
     getPointsFromApi()
-  }, [])
+  }, [isFocused])
 
   if (loading) {
     return <LoadAnimation />
@@ -52,14 +59,17 @@ export const Home = () => {
                 y: 0.9,
               }}
               coordinate={{
-                latitude: virus.latitude,
-                longitude: virus.longitude,
+                latitude: parseFloat(virus.latitude),
+                longitude: parseFloat(virus.longitude),
               }}
             />
           )
         })}
       </MapView>
       <LogoImage source={Logo} />
+      <FooterContainer>
+        <IconButton iconName="add" onPress={handleAdd} />
+      </FooterContainer>
     </HomeContainer>
   )
 }
