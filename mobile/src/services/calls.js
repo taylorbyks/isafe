1500218a6b011bbcaab07c37633dd72393713836
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-07-26 21:46:23 -0300

    Update services

--- mobile/src/services/calls.js
+++ mobile/src/services/calls.js
@@ -2,7 +2,7 @@ import api from './api'
 
 export async function getPoints() {
   try {
-    const result = await api.get('/points')
+    const result = await api.get('points')
 
     return result.data
   } catch (error) {
