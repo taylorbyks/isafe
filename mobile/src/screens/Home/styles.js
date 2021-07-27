5d79cd3ed6e8e39be0555adb96cbbbf9170e688e
Taylor Schinvelski <taylorbyk@hotmail.com>
2021-07-26 22:25:58 -0300

    Update styles

--- mobile/src/screens/Home/styles.js
+++ mobile/src/screens/Home/styles.js
@@ -1,17 +1,29 @@
 import styled, { css } from 'styled-components/native'
 
 export const HomeContainer = styled.View`
-  ${({ theme }) => css`
-    flex: 1;
-  `}
+  flex: 1;
+  align-items: center;
 `
 
 export const LogoImage = styled.Image`
   ${({ theme }) => css`
     position: absolute;
     top: ${theme.metrics.px(-10)}px;
-    left: ${theme.metrics.px(145)}px;
     width: ${theme.metrics.px(100)}px;
+    height: ${theme.metrics.px(100)}px;
+  `}
+`
+
+export const FooterContainer = styled.View`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.white};
+    border-radius: ${theme.metrics.px(15)}px;
+    align-items: center;
+    justify-content: flex-start;
+    padding: ${theme.metrics.px(15)}px;
+    position: absolute;
+    bottom: ${theme.metrics.px(-2)}px;
+    width: 100%;
     height: ${theme.metrics.px(100)}px;
   `}
 `
